71a900ea83e0df46408ec8c3d813c1884dffefd7
Sohib <jaynarovsohib@gmail.com>
2022-03-07 14:12:56 +0500

    Add project files.

---
+++ Models/Music.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playlist.Models
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int SingerID { get; set; }
+        public int Length { get; set; }
+        public int PlaylistID { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
---
+++ ViewModels/MusicView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playlist.Service.ViewModels
+{
+    public class MusicView
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Singer { get; set; }
+        public int Length { get; set; }
+        public string Playlist { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
